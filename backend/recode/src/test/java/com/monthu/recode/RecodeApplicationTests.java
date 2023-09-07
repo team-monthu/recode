fea6aa8669e0a3beec432829d12c91aa1408bea6
Jy0923 <pbj2y0923@naver.com>
2023-09-07 20:22:00 +0900

    init backend

---
+++ backend/recode/src/test/java/com/monthu/recode/RecodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.monthu.recode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
