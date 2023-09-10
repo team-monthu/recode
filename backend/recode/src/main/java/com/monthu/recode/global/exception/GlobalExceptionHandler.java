4eb8a9714cfeb4f37bb6f74a25eccb88c2d74120
Jy0923 <pbj2y0923@naver.com>
2023-09-11 00:11:23 +0900

    feat: 컨트롤러 어드바이스 추가

---
+++ backend/recode/src/main/java/com/monthu/recode/global/exception/GlobalExceptionHandler.java
@@ -0,0 +1,17 @@
+package com.monthu.recode.global.exception;
+
+import com.monthu.recode.global.dto.HttpResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(BusinessException.class)
+    public ResponseEntity<?> handleBusiness(BusinessException e) {
+        return HttpResponse.fail(HttpStatus.valueOf(e.getStatusCode()), e.getMessage());
+    }
+
+}
