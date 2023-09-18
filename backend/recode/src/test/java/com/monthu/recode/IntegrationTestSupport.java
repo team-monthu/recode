34e948b8b1d4d9742c8b88ad95ee7d06021c9d05
BaeYoungSuk <bysuk01@gmail.com>
2023-09-18 20:14:55 +0900

    chore: H2 의존성 추가 및 테스트 파일 수정

---
+++ backend/recode/src/test/java/com/monthu/recode/IntegrationTestSupport.java
@@ -0,0 +1,12 @@
+package com.monthu.recode;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@ActiveProfiles("test")
+@Transactional
+public abstract class IntegrationTestSupport {
+
+}
