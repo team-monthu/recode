15be51980390a92ecc962e6d7f755dbb8eefc9b6
BaeYoungSuk <bysuk01@gmail.com>
2023-09-25 20:36:49 +0900

    refactor: UseCase 인터페이스 제거 및 Service 클래스명 변경

--- backend/recode/src/main/java/com/monthu/recode/domain/feed/application/FeedUseCase.java
+++ backend/recode/src/main/java/com/monthu/recode/domain/feed/application/FeedUseCase.java
@@ -1,8 +1,29 @@
 package com.monthu.recode.domain.feed.application;
 
+import com.monthu.recode.domain.feed.application.repository.FeedRepository;
+import com.monthu.recode.domain.feed.domain.Feed;
 import com.monthu.recode.domain.feed.dto.WriteFeedReqDto;
 import com.monthu.recode.domain.feed.dto.WriteFeedResDto;
+import com.monthu.recode.domain.feed.exception.FeedNotFoundException;
+import com.monthu.recode.global.util.MarkdownUtil;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
-public interface FeedUseCase {
-  WriteFeedResDto writeFeed(WriteFeedReqDto writeFeedReqDto);
+@Service
+@RequiredArgsConstructor
+public class FeedUseCase {
+
+  private final FeedRepository feedRepository;
+  private final MarkdownUtil markdownUtil;
+
+  public WriteFeedResDto writeFeed(WriteFeedReqDto writeFeedReqDto) {
+    String htmlContent = markdownUtil.renderMarkdownToHtml(writeFeedReqDto.getContent());
+
+    Optional<Feed> feed = feedRepository.save(writeFeedReqDto.of(writeFeedReqDto, htmlContent));
+
+    return WriteFeedResDto.builder()
+        .feedId(feed.orElseThrow(FeedNotFoundException::new).getId())
+        .build();
+  }
 }
